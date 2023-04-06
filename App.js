0cf7b47c9bdc03ca2c076f3af6933ee9954d72b4
luisnandomp <luisnando.marques2107@gmail.com>
2023-04-05 22:25:52 -0300

    Meu primeiro APP em react

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titulo}>Olá Mundo!</Text>
+
+      <Text style={styles.texto}>Olá meu nome é Luis e esse é meu primeiro APP em React :D</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#Ccc',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 30,
+    padding: 30,
+    marginBottom: 300,
+    backgroundColor: '#FF69B4',
+    borderRadius: 10,
+    color: 'red',
+  },
+  texto: {
+    color: 'blue',
+    fontSize: 20,
+
+  }
 });
